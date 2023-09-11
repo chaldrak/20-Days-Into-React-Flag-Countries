66dc98b562d80de59caab7422c6eec707bd021c1
AzizVorrez <azizvorrez8@gmail.com>
2023-09-12 00:10:37 +0100

    Exo rendu

--- src/composants/SingleBouton.jsx
+++ src/composants/SingleBouton.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import Countries from '/src/constants/countries.json'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
+// Importez les données des pays depuis le fichier JSON
+import Countries from '/src/constants/countries.json';
 
-const CountriesButtons = ({ onclick }) => {
-    
-    const countrieKey = (Object.keys(Countries))
-    return (
+// Importez le CSS nécessaire ici
 
-        <div className = "btnDiv">
-            {
-                countrieKey.map((item, idx) => (
-                    <button onClick = { () => onclick (item) } className = "btn" type="button" key= {idx}> {item} </button>
-                )
+const CountriesButtons = ({ onClick }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
-                )
-                }
+  const handleClick = (countryName) => {
+    // Créez le chemin de l'image du drapeau en fonction du nom du pays
+    const flagImagePath = `/src/assets/png1000px/${countryName.toLowerCase()}.png`;
+
+    // Mettez à jour l'état avec le nom du pays et le chemin de l'image du drapeau
+    setSelectedCountry({ name: countryName, flag: flagImagePath });
+
+    // Appellez la fonction onClick passée en prop pour effectuer d'autres actions si nécessaire
+    onClick(countryName);
+  };
+
+  return (
+    <div>
+        <div className="btnDiv">
+            {Object.keys(Countries).map((countryName, idx) => (
+            <button
+            onClick={() => handleClick(countryName)}
+            className="btn"
+            type="button"
+            key={idx}
+            >
+            {countryName}
+            </button>
+            ))}
         </div>
-        
-        
-    )
+        <div>
+            {selectedCountry && (
+            <div className="selectedCountry">
+            <img src={selectedCountry.flag} alt={selectedCountry.name} />
+            <p>{selectedCountry.name}</p>
+            </div>
+        )}
+        </div>
+    </div>
+  );
+};
 
-}
-
-export default CountriesButtons
+export default CountriesButtons;
